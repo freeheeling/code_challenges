08209756d325477824e515b97258ed56bc12eb3f
Scott Regenthal <scottregenthal@gmail.com>
2020-05-26 07:12:12 -0600

    Devise additional solutions

--- codewars/multiples_of_3_or_5/multiples_of_3_or_5.rb
+++ codewars/multiples_of_3_or_5/multiples_of_3_or_5.rb
@@ -12,9 +12,28 @@ def solution(number)
   sum
 end
 
-# p solution(10)
+def solution(number)
+  sum = 0
+  for n in 1...number
+    if n % 3 == 0 || n % 5 == 0
+      sum += n
+    end
+  end
+  sum
+end
+
+def solution(number)
+  multiples = (1...number).select { |num|
+    num % 3 == 0 || num % 5 == 0
+  }
+  multiples.sum
+end
+
+puts " Input: 10 \n Output: #{solution(10)}"
+puts "\n"
 # 23
-# p solution(20)
-# # 78
-p solution(200)
+puts " Input: 20 \n Output: #{solution(20)}"
+# 78
+puts "\n"
+puts " Input: 200 \n Output: #{solution(200)}"
 # 9168
